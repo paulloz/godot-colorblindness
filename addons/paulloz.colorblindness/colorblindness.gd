ab3d5658339e094b5b6e496af33d19a771c8463d
Paul Joannon <hello@pauljoannon.com>
2019-03-27 13:53:53 +0100

    colorblindness node now inherits node and not colorrect anymore

--- addons/paulloz.colorblindness/colorblindness.gd
+++ addons/paulloz.colorblindness/colorblindness.gd
@@ -1,15 +1,19 @@
 tool
-extends ColorRect
+extends Node
 
 enum TYPE { None, Protanopia, Deuteranopia, Tritanopia, Achromatopsia }
 
 export(TYPE) onready var Type = TYPE.None setget set_type
 
+var rect = ColorRect.new()
+
 func set_type(value):
-    if self.material:
-        self.material.set_shader_param("type", value)
+    if rect.material:
+        rect.material.set_shader_param("type", value)
         Type = value
 
 func _ready():
-    self.material = load("res://addons/paulloz.colorblindness/colorblindness.material")
-    self.rect_min_size = get_viewport_rect().size
+    self.add_child(rect)
+
+    rect.rect_min_size = rect.get_viewport_rect().size
+    rect.material = load("res://addons/paulloz.colorblindness/colorblindness.material")
