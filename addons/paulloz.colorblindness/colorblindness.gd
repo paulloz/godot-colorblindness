a52b4b50afa7a99ccaa347cd879ba9588a1ef44c
Paul Joannon <hello@pauljoannon.com>
2020-05-11 23:29:08 +0200

    fix timing issue with 3D

--- addons/paulloz.colorblindness/colorblindness.gd
+++ addons/paulloz.colorblindness/colorblindness.gd
@@ -5,16 +5,23 @@ extends Node
 enum TYPE { None, Protanopia, Deuteranopia, Tritanopia, Achromatopsia }
 
 export(TYPE) onready var Type = TYPE.None setget set_type
+var temp = null
 
 var rect = ColorRect.new()
 
 func set_type(value):
     if rect.material:
         rect.material.set_shader_param("type", value)
-        Type = value
+    else:
+        temp = value
+    Type = value
 
 func _ready():
-    self.add_child(rect)
+    self.add_child(self.rect)
 
-    rect.rect_min_size = rect.get_viewport_rect().size
-    rect.material = load("res://addons/paulloz.colorblindness/colorblindness.material")
+    self.rect.rect_min_size = self.rect.get_viewport_rect().size
+    self.rect.material = load("res://addons/paulloz.colorblindness/colorblindness.material")
+    if self.temp:
+        self.Type = self.temp
+        self.temp = null
+
