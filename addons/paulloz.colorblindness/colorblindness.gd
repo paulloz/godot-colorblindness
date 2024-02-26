7fc17efb1b05fc1c1faf0a322099068f969519f8
Dave <el-falso@users.noreply.github.com>
2024-02-26 13:18:04 +0100

    Migrate to Godot 4.2.1 (#5)

--- addons/paulloz.colorblindness/colorblindness.gd
+++ addons/paulloz.colorblindness/colorblindness.gd
@@ -1,32 +1,35 @@
-tool
-class_name Colorblindness, "res://addons/paulloz.colorblindness/colorblindness.svg"
+@tool
+@icon("res://addons/paulloz.colorblindness/colorblindness.svg")
+class_name Colorblindness
 extends CanvasLayer
 
 enum TYPE { None, Protanopia, Deuteranopia, Tritanopia, Achromatopsia }
 
-export(TYPE) onready var Type = TYPE.None setget set_type
+@export
+var Type: TYPE = TYPE.None:
+	set(value):
+		if rect.material:
+			rect.material.set_shader_parameter("type", value)
+		else:
+			temp = value
+		Type = value
+		
 var temp = null
 
-var rect = ColorRect.new()
+var rect := ColorRect.new()
 
-func set_type(value):
-    if rect.material:
-        rect.material.set_shader_param("type", value)
-    else:
-        temp = value
-    Type = value
 
 func _ready():
-    self.add_child(self.rect)
+	self.add_child(self.rect)
 
-    self.rect.rect_min_size = self.rect.get_viewport_rect().size
-    self.rect.material = load("res://addons/paulloz.colorblindness/colorblindness.material")
-    self.rect.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
-    if self.temp:
-        self.Type = self.temp
-        self.temp = null
+	self.rect.custom_minimum_size = self.rect.get_viewport_rect().size
+	self.rect.material = load("res://addons/paulloz.colorblindness/colorblindness.material")
+	self.rect.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
+	if self.temp:
+		self.Type = self.temp
+		self.temp = null
 
-    self.get_tree().root.connect('size_changed', self, '_on_viewport_size_changed')
+	self.get_tree().root.size_changed.connect(_on_viewport_size_changed)
 
 func _on_viewport_size_changed():
-    self.rect.rect_min_size = self.rect.get_viewport_rect().size
+	self.rect.rect_min_size = self.rect.get_viewport_rect().size
